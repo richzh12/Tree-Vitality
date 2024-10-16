7880069ac85575a51cdf72a7c1fb171dc8512bdd
richzh12 <richardzhang40@gmail.com>
2024-10-15 20:53:12 -0500

    dropdown al nav y nueva seccion para reportes

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,30 +1,135 @@
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
-import styles from './nav.module.css';
 
 const nav = () => {
+    // Control del despliegue del dropdown
+    const toggleDropdown = () => {
+      const dropdownMenu = document.getElementById("dropdownNavbar");
+      if (dropdownMenu.classList.contains("hidden")) {
+        dropdownMenu.classList.remove("hidden");
+      } else {
+        dropdownMenu.classList.add("hidden");
+      }
+    };
+  
     return (
-        <div className={`${styles.nav} fixed top-0 left-0 w-full z-50 bg-white shadow-md`}>
-            <nav className="max-w-6xl mx-auto px-4 py-2">
-                <ul className="flex space-x-4">
-                    <li>
-                        <Link href="/" className="text-gray-800 hover:text-blue-600">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link href="/reporte-page" className="text-gray-800 hover:text-blue-600">Reportar</Link>
-                    </li>
-                    <li>
-                        <Link href="/actividades-page" className="text-gray-800 hover:text-blue-600">Actividades</Link>
-                    </li>
-                    <li>
-                        <Link href="/contactenos-page" className="text-gray-800 hover:text-blue-600">Contáctenos</Link>
-                    </li>
-                    <li>
-                        <Link href="/auth-page/login" className="text-gray-800 hover:text-blue-600">Iniciar Sesion</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+<nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+    <button
+      data-collapse-toggle="navbar-dropdown"
+      type="button"
+      class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      aria-controls="navbar-dropdown"
+      aria-expanded="false"
+    >
+      <span class="sr-only">Open main menu</span>
+      <svg
+        class="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
+      </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <Link
+            href="/"
+            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+            aria-current="page"
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <button
+            id="dropdownNavbarLink"
+            onClick={toggleDropdown}
+            class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+          >
+            Reporte
+            <svg
+              class="w-2.5 h-2.5 ms-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          <div
+            id="dropdownNavbar"
+            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+          >
+            <ul
+              class="py-2 text-sm text-gray-700 dark:text-gray-400"
+              aria-labelledby="dropdownLargeButton"
+            >
+              <li>
+                <Link
+                  href="../../reporte-page/reportar"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Hacer un reporte
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="../../reporte-page/reportes"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Ver reportes
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <Link
+            href="../../actividades-page"
+            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            Actividades
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="../../contactenos-page"
+            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            Contáctenos
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="../../auth-page/login"
+            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+          >
+            Iniciar Sesión
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+);
 };
 
 export default nav;
